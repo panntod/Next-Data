fde5f9f9523805dc1c3f2b6ea4d0da5f723a27c7
panntod <munjalindra.pandhu@gmail.com>
2025-01-15 14:54:05 +0700

    refactor: Make className prop default to an empty string in MainWrapper for improved usability

--- src/components/Layout/component/MainWrapper.tsx
+++ src/components/Layout/component/MainWrapper.tsx
@@ -1,7 +1,7 @@
 import cn from '@/lib/clsx'
 import { SectionWrapperProps } from '../types/Wrapper.types'
 
-export function MainWrapper({ className, id, children }: Readonly<SectionWrapperProps>) {
+export function MainWrapper({ className = '', id, children }: Readonly<SectionWrapperProps>) {
   return (
     <main id={id} className={cn('container mx-auto md:max-w-7xl min-h-screen', className)}>
       {children}
