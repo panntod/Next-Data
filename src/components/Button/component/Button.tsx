af8b3848f08b9e1efc6ff7e816c461d9787c7d32
panntod <munjalindra.pandhu@gmail.com>
2025-01-20 12:00:28 +0700

    feat: Enhance Button component with support for children and additional variant styles

--- src/components/Button/component/Button.tsx
+++ src/components/Button/component/Button.tsx
@@ -1,18 +1,20 @@
 'use client'
 
 import cn from '@/lib/clsx'
+import { PropsWithChildren } from 'react'
 import { ButtonProps } from '../types/Button.types'
 import { buttonVariants } from '../utils'
 
 export function Button({
   type = 'button',
   onClick,
-  text = 'Button',
+  text,
   isDisabled = false,
   className = '',
   variant = 'primary',
-  size = 'medium'
-}: Readonly<ButtonProps>) {
+  size = 'medium',
+  children
+}: PropsWithChildren<ButtonProps>) {
   return (
     <button
       type={type}
@@ -20,7 +22,7 @@ export function Button({
       disabled={isDisabled}
       className={cn(buttonVariants({ variant, size }), className)}
     >
-      {text}
+      {text || children}
     </button>
   )
 }
--- src/components/Button/utils/index.tsx
+++ src/components/Button/utils/index.tsx
@@ -2,18 +2,32 @@ import { cva } from 'class-variance-authority'
 
 export const buttonVariants = cva('transition-all duration-300 text-light hover:text-light', {
   variants: {
+    // Variants for colors (primary, secondary, danger, warning, success)
     variant: {
-      'text-primary': 'text-primary-blue hover:bg-primary-blue',
       primary: 'bg-primary-blue hover:bg-secondary-blue',
-      secondary: 'bg-secondary-blue',
+      secondary: 'bg-secondary-blue hover:bg-secondary-blue',
       danger: 'bg-red-500 hover:bg-red-700',
       warning: 'bg-yellow-500 hover:bg-yellow-700',
-      success: 'bg-green-500 hover:bg-green-700'
+      success: 'bg-green-500 hover:bg-green-700',
+
+      // Additional color styles for outlined buttons
+      'primary-outline': 'border border-primary-blue text-primary-blue hover:bg-primary-blue hover:text-white',
+      'secondary-outline': 'border border-secondary-blue text-secondary-blue hover:bg-secondary-blue hover:text-white',
+      'danger-outline': 'border border-red-500 text-red-500 hover:bg-red-500 hover:text-white',
+      'warning-outline': 'border border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-white',
+      'success-outline': 'border border-green-500 text-green-500 hover:bg-green-500 hover:text-white',
+
+      // Additional color styles for plain buttons
+      'primary-plain': 'text-primary-blue hover:text-secondary-blue',
+      'secondary-plain': 'text-secondary-blue hover:text-primary-blue',
+      'danger-plain': 'text-red-500 hover:text-red-700',
+      'warning-plain': 'text-yellow-500 hover:text-yellow-700',
+      'success-plain': 'text-green-500 hover:text-green-700'
     },
     size: {
       small: 'px-[16px] py-[8px] text-sm rounded-[6px]',
-      medium: 'px-[20px] md:px-[24px] py-[12px] md:py-[16px] text-base rounded-[12px]',
-      large: 'px-[28] md:px-[32px] py-[16px] md:py-[20px] text-lg rounded-[16px]'
+      medium: 'px-[20px] md:px-[22px] py-[10px] md:py-[12px] text-base rounded-[8px]',
+      large: 'px-[24] md:px-[26px] py-[14px] md:py-[16px] text-lg rounded-[10px]'
     }
   },
   defaultVariants: {
