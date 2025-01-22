9b33178633e7214ce46e8a1086a5e4d80166f9e1
panntod <munjalindra.pandhu@gmail.com>
2025-01-22 10:08:52 +0700

    feat: Add motion animations to Card component for improved visibility on scroll

--- src/components/Card/component/Card.tsx
+++ src/components/Card/component/Card.tsx
@@ -1,7 +1,27 @@
+'use client'
+
 import cn from '@/lib/clsx'
+import { motion, useInView } from 'motion/react'
+import { useRef } from 'react'
 import { CardProps } from '../types/Card.types'
 import { cardVariants } from '../utils'
 
 export function Card({ children, className = '', size }: Readonly<CardProps>) {
-  return <article className={cn(cardVariants({ size }), className)}>{children}</article>
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+
+  return (
+    <motion.article
+      ref={ref}
+      className={cn(cardVariants({ size }), className)}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{
+        opacity: isInView ? 1 : 0,
+        y: isInView ? 0 : 20
+      }}
+      transition={{ ease: 'easeInOut', duration: 1 }}
+    >
+      {children}
+    </motion.article>
+  )
 }
