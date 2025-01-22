86c1d00a8456494e2ea6e16cc2558fdd3c7aa5ea
panntod <munjalindra.pandhu@gmail.com>
2025-01-22 09:59:17 +0700

    feat: Add Counter component with animated counting functionality

---
+++ src/components/Typography/component/Counter.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import cn from '@/lib/clsx'
+import { animate, motion, useInView, useMotionValue, useTransform } from 'motion/react'
+import { useEffect, useRef } from 'react'
+import { SIZE_VARIANT } from '../constant/SIZE'
+import { CounterProps } from '../types/Counter.types'
+
+export function Counter({ endCount = 100, duration = 5, size = 'BODY_MD_NORMAL', className = '' }: CounterProps) {
+  const { className: sizeClassName } = SIZE_VARIANT[size]
+  const count = useMotionValue(0)
+  const ref = useRef(null)
+
+  const rounded = useTransform(count, value => Math.round(value))
+  const isInView = useInView(ref, { once: true }) // Trigger hanya sekali
+
+  useEffect(() => {
+    if (isInView) {
+      const controls = animate(count, endCount, { duration })
+      return () => controls.stop()
+    }
+  }, [isInView])
+
+  return (
+    <motion.pre ref={ref} className={cn(sizeClassName, className)}>
+      {rounded}
+    </motion.pre>
+  )
+}
