856380e5c4f13a1b940f9ade8764f17ed33ffe25
Gamer2Language <bagassatwi@gmail.com>
2025-06-06 16:37:54 +0700

    refactor: Remove console logs from user page component

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,9 +1,7 @@
 import { getUsers } from './actions'
 import UsersPage from './user'
 const UserPage = async ({ searchParams }: { searchParams: { page?: string } }) => {
-  console.log('searchParams', searchParams)
   const page = parseInt(searchParams.page || '1', 10)
-  console.log(page)
 
   const users = await getUsers({ page })
   return <UsersPage user={users} />
