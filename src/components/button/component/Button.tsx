f0cbe438292ef47f758c9b952a95b0579bf3cb5f
panntod <munjalindra.pandhu@gmail.com>
2025-06-20 09:50:18 +0700

    refactor: Update Button component add disabled state and types for improved readability and consistency

--- src/components/button/component/Button.tsx
+++ src/components/button/component/Button.tsx
@@ -1,28 +1,32 @@
-'use client'
+"use client";
 
-import cn from '@/lib/clsx'
-import { PropsWithChildren } from 'react'
-import { ButtonProps } from '../types/Button.types'
-import { buttonVariants } from '../utils'
+import cn from "@/lib/clsx";
+import { PropsWithChildren } from "react";
+import { ButtonProps } from "../types/Button.types";
+import { buttonVariants } from "../utils";
 
 export function Button({
-  type = 'button',
+  type = "button",
   onClick,
   text,
   isDisabled = false,
-  className = '',
-  variant = 'primary',
-  size = 'medium',
-  children
+  className = "",
+  variant = "primary",
+  size = "medium",
+  fullWidth = false,
+  children,
+  ...props
 }: PropsWithChildren<ButtonProps>) {
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={isDisabled}
+      {...props}
+      style={{ width: fullWidth ? "100%" : "auto" }}
       className={cn(buttonVariants({ variant, size }), className)}
     >
       {text || children}
     </button>
-  )
+  );
 }
--- src/components/button/types/Button.types.ts
+++ src/components/button/types/Button.types.ts
@@ -1,10 +1,13 @@
-import { type VariantProps } from 'class-variance-authority'
-import { ComponentPropsWithoutRef } from 'react'
-import { buttonVariants } from '../utils'
+import { type VariantProps } from "class-variance-authority";
+import { ComponentPropsWithoutRef } from "react";
+import { buttonVariants } from "../utils";
 
-export interface ButtonProps extends ComponentPropsWithoutRef<'button'>, VariantProps<typeof buttonVariants> {
-  text?: string
-  type?: 'button' | 'reset' | 'submit'
-  isDisabled?: boolean
-  className?: string
+export interface ButtonProps
+  extends ComponentPropsWithoutRef<"button">,
+    VariantProps<typeof buttonVariants> {
+  fullWidth?: boolean;
+  text?: string;
+  type?: "button" | "reset" | "submit";
+  isDisabled?: boolean;
+  className?: string;
 }
